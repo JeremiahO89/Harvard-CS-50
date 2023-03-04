bc653862c6eff6a789a9d98a095402923e89bc10
JeremiahO89 <technologyfrog89@gmail.com>
2023-03-04 11:55:06 -0500

    Final commit

---
+++ C/caculatior.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main (void)
+{
+        int points= get_int("how many points did you loose?");
+
+        if (points < 2)
+        {
+                printf("you lost fewer than me. \n");
+        }
+        else if (points > 2)
+        {
+                printf("you lost more points than me. \n");
+        }
+        else (points = 2)
+        {
+                printf("we lost the same number of points \n");
+        }
